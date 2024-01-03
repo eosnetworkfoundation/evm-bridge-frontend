e6b9c538611e250bb627b220c21d691ee33d178e
yarkin <yarkin.who@gmail.com>
2024-01-03 17:06:29 +0800

    Refactor how to present ingress fee.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -73,8 +73,8 @@
                 <div class="mb-3">
                   <label for="address">{{ $t('home.destinationAccount') }}</label>
                   <div class="form-text mb-2">{{ $t('home.destinationAccountDesc') }}</div>
-                  <input type="text" id="address" class="form-control select" v-model="targetAddress"
-                    maxlength="13" list="addresses">
+                  <input type="text" id="address" class="form-control select" v-model="targetAddress" maxlength="13"
+                    list="addresses">
                   <div class="form-text font-monospace" v-if='extraWarning !== ""'>
                     <span style="color: red">{{ extraWarning }}</span>
                   </div>
@@ -161,6 +161,26 @@
               <p>{{ $t('home.eos2evmDesc.p2') }}</p>
               <p v-if="env === 'TESTNET'">{{ $t('home.eos2evmDesc.p3.testnet') }}</p>
               <p v-else>{{ $t('home.eos2evmDesc.p3.mainnet') }}</p>
+            <table class="table table-striped" style="max-width: 16em;">
+                <thead>
+                  <tr>
+                    <th>Token</th>
+                    <th>Ingress fee</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(item, index) in tokenList">
+                    <td>{{item.name}}</td>
+                    <td>{{item.ingressFee}} {{item.name}}</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div v-if="env === 'TESTNET'" v-html="$t('home.eos2evmDesc.p4.testnet', { 'interpolation': {'escapeValue': false} })"></div>
+              <div v-else v-html="$t('home.eos2evmDesc.p4.mainnet', { 'interpolation': {'escapeValue': false} })"></div>
+              <div ></div>
+
+              
             </div>
           </b-row>
         </b-card>
@@ -213,9 +233,6 @@
         </div>
 
         <div class="mt-2 text-center small text-white">
-          {{ $t('home.bridgeFee') }}
-          <span>0.01 EOS</span>
-          <br>
           {{ $t('home.transferTime', ['~ 5 s']) }}
         </div>
       </b-tab>
@@ -262,42 +279,58 @@ export default {
       egressFee: '0',
       decimals: null,
       tokenListTestnet: [
-        { name: 'EOS', addr: '', logo: 'images/eos.png' },
-        { name: 'JUNGLE', addr: '0x4ea3b729669bF6C34F7B80E5D6c17DB71F89F21F', logo: 'images/jungle.png', erc20_contract: null },
+        { name: 'EOS', addr: '', logo: 'images/eos.png', ingressFee: 0.01 },
+        { name: 'JUNGLE', addr: '0x4ea3b729669bF6C34F7B80E5D6c17DB71F89F21F', logo: 'images/jungle.png', erc20_contract: null, ingressFee: 0.01 },
       ],
       tokenListMainnet: [
-        { name: 'EOS', addr: '', logo: 'images/eos.png', 
-        blockList:  ['eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos'], 
-        warningList: ['huobideposit', 'okbtothemoon', 'gateiowallet', 'coinbasebase', 'krakenkraken'] 
-      },
-        { name: 'USDT', addr: '0x33B57dC70014FD7AA6e1ed3080eeD2B619632B8e', logo: 'images/usdt.png' , 
-        blockList:  ['eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: ['gateiowallet'] 
-      },
-      { name: 'ZEOS', addr: '0x477F09A0bDb273C8933429109fEBd3c3b0388B8A', logo: 'images/zeos.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
-      { name: 'BRAM', addr: '0x102F21abC12eBD194259C1081B13916192E7cBe5', logo: 'images/bram.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
-      { name: 'SEOS', addr: '0xbfb10f85b889328e4a42507e31a07977ae00eec6', logo: 'images/seos.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
-      { name: 'BOX', addr: '0x9b3754f036de42846e60c8d8c89b18764f168367', logo: 'images/box.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
-      { name: 'USN', addr: '0x8d0258d6ccfb0ce394dc542c545566936b7974f9', logo: 'images/usn.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
-      { name: 'BANANA', addr: '0xC500C831AF8a5d1F4f3B1fc3940175A8db68C3CB', logo: 'images/banana.png' , 
-        blockList:  ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'], 
-        warningList: [] 
-      },
+        {
+          name: 'EOS', addr: '', logo: 'images/eos.png',
+          blockList: ['eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos'],
+          warningList: ['huobideposit', 'okbtothemoon', 'gateiowallet', 'coinbasebase', 'krakenkraken'],
+          ingressFee: 0.01
+        },
+        {
+          name: 'USDT', addr: '0x33B57dC70014FD7AA6e1ed3080eeD2B619632B8e', logo: 'images/usdt.png',
+          blockList: ['eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: ['gateiowallet'],
+          ingressFee: 0.01
+        },
+        {
+          name: 'ZEOS', addr: '0x477F09A0bDb273C8933429109fEBd3c3b0388B8A', logo: 'images/zeos.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 20
+        },
+        {
+          name: 'BRAM', addr: '0x102F21abC12eBD194259C1081B13916192E7cBe5', logo: 'images/bram.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 1000
+        },
+        {
+          name: 'SEOS', addr: '0xbfb10f85b889328e4a42507e31a07977ae00eec6', logo: 'images/seos.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 0.1
+        },
+        {
+          name: 'BOX', addr: '0x9b3754f036de42846e60c8d8c89b18764f168367', logo: 'images/box.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 0.1
+        },
+        {
+          name: 'USN', addr: '0x8d0258d6ccfb0ce394dc542c545566936b7974f9', logo: 'images/usn.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 0.1
+        },
+        {
+          name: 'BANANA', addr: '0xC500C831AF8a5d1F4f3B1fc3940175A8db68C3CB', logo: 'images/banana.png',
+          blockList: ['gateiowallet', 'eosbndeposit', 'bybitdeposit', 'bitgeteosdep', 'kucoindoteos', 'binancecleos', 'coinbasebase', 'krakenkraken', 'huobideposit', 'okbtothemoon'],
+          warningList: [],
+          ingressFee: 20
+        },
       ],
     }
   },
@@ -403,7 +436,7 @@ export default {
         amount = s[0]
         extraExp = Number(s[1]);
       }
-      
+
       const denomination = BigInt(10) ** BigInt(inputDeciamls)
       // From web3.js
       // if value is decimal e.g. 24.56 extract `integer` and `fraction` part
@@ -422,14 +455,14 @@ export default {
         result = result.padEnd(result.length + extraExp, '0')
       }
       else if (extraExp < 0) {
-        result = result.slice(0, extraExp).padStart(1,"0")
+        result = result.slice(0, extraExp).padStart(1, "0")
       }
 
       if (fraction.length === 0) {
         return result.toString();
       }
 
-      return result.slice(0, -fraction.length).padStart(1,"0");
+      return result.slice(0, -fraction.length).padStart(1, "0");
     },
 
     async getPrice() {
@@ -516,9 +549,9 @@ export default {
           this.decimals = await this.erc20_contract().read.decimals()
           this.balance = this.displayValue(wei.toString(), this.decimals)
         }
-        else { 
+        else {
           this.balance = null;
-          this.decimals = null; 
+          this.decimals = null;
         }
       }
 
@@ -532,7 +565,7 @@ export default {
       else {
         this.egressFee = '0'
       }
-      
+
       this.getBalance()
     },
 
@@ -568,7 +601,7 @@ export default {
         var vm = this
 
         let tx = null;
-        
+
         await this.getPrice()
 
         if (this.tokenName() === 'EOS') {
@@ -809,5 +842,4 @@ export default {
 
 .error {
   color: #fff;
-}
-</style>
+}</style>
